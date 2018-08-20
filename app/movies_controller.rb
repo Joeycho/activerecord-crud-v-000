42407b62fe74492fb81f793b8ff06764a9662ae5
Joey Cho <cjy12182000@naver.com>
2018-08-20 04:20:31 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,9 +6,9 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = Movie.new
   movie.title = "This is a title."
-  __
+  movie.save
 end
 
 def can_be_created_with_a_hash_of_attributes
@@ -20,33 +20,50 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie = Movie.new
+  movie.title = attributes[:title]
+  movie.release_date = attributes[:release_date]
+  movie.director = attributes[:director]
+  movie.lead = attributes[:lead]
+  movie.in_theaters = attributes[:in_theaters]
+  movie
+  binding.pry
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(title: "Home Alone", release_date: 1990)
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
-  
+  #binding.pry
+
   Movie.create do |m|
-    __
+    m.title = title
+    m.release_date = release_date
   end
+
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  movie = Movie.find_by_sql("SELECT * FROM movies
+  LIMIT 1")
+  return movie[0]
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  movie = Movie.find_by_sql("SELECT * FROM movies
+  ")
+  movie[-1]
 end
 
 def can_get_size_of_the_database
-  __
+  s = Movie.find_by_sql("SELECT * FROM movies")
+  s.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  movie = Movie.find_by_sql("SELECT * FROM movies
+  LIMIT 1")
+  return movie[0]
 end
 
 def can_find_by_multiple_attributes
@@ -54,11 +71,11 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  
 end
 
 def can_find_using_where_clause_and_be_sorted
-  # For this test return all movies released after 2002 and ordered by 
+  # For this test return all movies released after 2002 and ordered by
   # release date descending
   __
 end

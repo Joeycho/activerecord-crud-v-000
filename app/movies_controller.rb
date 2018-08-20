4e9cadcb23b842b4c35967dd6ed5b8c987b2502b
Joey Cho <cjy12182000@naver.com>
2018-08-20 05:01:44 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -109,7 +109,7 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  Movie.destroy(0)
+  Movie.destroy(1)
 end
 
 def can_destroy_all_items_at_once
